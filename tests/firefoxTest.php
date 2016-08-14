821e113ea6699dabe5f96cd3e4248d0806e23c6b
vagrant <vagrant@localhost.localdomain>
2016-08-14 06:32:46 +0000

    Initial commit.

---
+++ tests/firefoxTest.php
@@ -0,0 +1,27 @@
+<?php
+use Facebook\WebDriver;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+ 
+class firefoxTest extends PHPUnit_Framework_TestCase 
+{
+    public function testWebUI()
+    {
+        // ドライバーの起動
+        $host = 'http://localhost:4444/wd/hub';
+        $driver = RemoteWebDriver::create($host, DesiredCapabilities::firefox());
+ 
+        // テストページへ遷移
+        $driver->get('https://www.google.co.jp/');
+ 
+        // 「Google」というタイトルを取得できることを確認する
+        $title = $driver->getTitle();
+        $this->assertEquals("Google", $title);
+ 
+        //ブラウザを閉じる
+        $driver->close();
+    }
+}
